da4d872a30ec87e625dec6db6673706102856165
Romerio Ribeiro <romerio111@hotmail.com>
2016-12-07 19:36:12 -0300

    Novo commit

---
+++ resources/app/lib/index.js
@@ -0,0 +1,20 @@
+/*
+'use strict'; 
+
+var app = require('electron').remote.app;
+
+var remote = require('electron');
+
+$(document).ready(function(){  
+    
+    'use strict';
+    var app = require('electron').remote.app;
+    var remote = require('electron');
+    
+    
+    //remote.BrowserWindow.getFocusedWindow().close();
+    $('.btn').click(function() {
+       alert('ok');
+    });
+    
+}); */
